c1988aaad3d4d1c4a32316c339f0ed5b2329d816
Rizal Gian Febriantama <rizal.febriantama@tm.idu.ac.id>
2024-10-31 09:07:07 +0700

    Revisi footer
    Changes to be committed:
    modified:   views/admin_dashboard.php
    modified:   views/lecturer_dashboard.php
    modified:   views/student_dashboard.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,8 @@
 <!-- includes/footer.php -->
 </div> <!-- End of content-container -->
-<footer class="main-footer">
-    <p>&copy; 2024 SIAKAD. All Rights Reserved.</p>
-</footer>
+    <footer class="main-footer">
+        <p>&copy; 2024 SIAKAD. All Rights Reserved.</p>
+    </footer>
 </body>
 </html>
 
