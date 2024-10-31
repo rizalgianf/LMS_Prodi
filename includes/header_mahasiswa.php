dcd029a770453c14af1b61d0fc498619723b25b4
Rizal Gian Febriantama <rizal.febriantama@tm.idu.ac.id>
2024-10-31 08:38:10 +0700

    Changes to be committed: modified:   views/admin_dashboard.php modified:   views/lecturer_dashboard.php modified:   views/student_dashboard.php
    Changes to be committed:
    modified:   views/admin_dashboard.php
    modified:   views/lecturer_dashboard.php
    modified:   views/student_dashboard.php

---
+++ includes/header_mahasiswa.php
@@ -0,0 +1,24 @@
+<!-- includes/header_mahasiswa.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $page_title; ?></title>
+    <link rel="stylesheet" href="../../css/style_mahasiswa.css">
+</head>
+<body>
+    <header class="main-header">
+        <div class="header-container">
+            <h1 class="site-title">SIAKAD Mahasiswa Dashboard</h1>
+            <nav class="main-nav">
+                <ul>
+                    <li><a href="home.php">Home</a></li>
+                    <li><a href="jadwal_kuliah.php">Jadwal Kuliah</a></li>
+                    <li><a href="nilai.php">Nilai Saya</a></li>
+                    <li><a href="absensi.php">Absensi Saya</a></li>
+                    <li><a href="../../logout.php">Logout</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <div class="content-container">
