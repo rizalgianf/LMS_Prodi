345cf8c078e1ad069290c507fe64245fa3997af1
Rizal Gian Febriantama <rizal.febriantama@tm.idu.ac.id>
2025-01-10 09:56:16 +0700

    new

--- includes/header_admin.php
+++ includes/header_admin.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title><?php echo $page_title; ?></title>
-    <link rel="stylesheet" href="../../css/style_admin.css">
+    <link rel="stylesheet" href="../../css/style_header.css">
 </head>
 <body>
     <header class="main-header">
